61b01ee17fb03702bd2e8f7a09acdc035f27978a
Martinez Vasquez <l.martinez.vasquez@accenture.com>
2018-08-27 12:15:00 -0300

    Commit 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,11 @@ export class AppComponent {
   msg: String = '';
 
   farms = [
-  {'id': '6', 'renspa': '01.023.3.0123', 'name': 'farm 6'},
-  {'id': '7', 'renspa': '01.023.3.0124', 'name': 'farm 7'},
-  {'id': '8', 'renspa': '01.023.3.0125', 'name': 'farm 8'}
+  {'id': '6', 'renspa': '01.023.3.00123', 'name': 'El Amanecer'},
+  {'id': '7', 'renspa': '01.023.3.00124', 'name': 'Super Pollo'},
+  {'id': '8', 'renspa': '01.023.3.00125', 'name': 'Seis Arroyos'}
   ];
-  
+
   model: any = {};
   model2: any = {};
   hideUpdate: boolean = true;
@@ -25,16 +25,16 @@ export class AppComponent {
   }
 
   deleteFarm(i): void {
-    var answer = confirm('Are you sure?')
-    if(answer){
+    var answer = confirm('Are you sure?');
+    if (answer) {
       this.farms.splice(i, 1);
       this.msg = 'Farm deleted';
     }
-    
+
   }
-  
-  myValue;
-  
+
+myValue;
+
   editFarm(i): void {
     this.hideUpdate = false;
     this.model2.id = this.farms[i].id;
@@ -42,11 +42,11 @@ export class AppComponent {
     this.model2.name = this.farms[i].name;
     this.myValue = i;
   }
-  
+
   updateFarm(): void {
     let i = this.myValue;
-    for (let j = 0; j < this.farms.length; j++){
-      if(i == j){
+    for (let j = 0; j < this.farms.length; j++) {
+      if (i === j) {
         this.farms[i] = this.model2;
         this.model2 = {};
         this.msg = 'Farm updated';
@@ -54,11 +54,11 @@ export class AppComponent {
       }
     }
   }
-  
+
   closeAlert(): void {
     this.msg = '';
   }
-  
+
 
 
 }
